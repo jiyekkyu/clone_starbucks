1b22121134e310609c94647ec66c8625803e9a9a
leeji5670 <leeji3063@naver.com>
2021-07-05 19:12:04 +0900

    starbucks menu add

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,38 @@
 <template>
-  <div class="home">
-  </div>
+	<div class="home">
+		<div class="sub-wrap">
+			<div class="sub-wrap-1">
+				<a class="starbucks-icon" href="/starbucks.co.kr">
+					<img :src="require('@/assets/images/starbucks_icon.png')">
+				</a>
+				<nav class="nav1">
+					<ul>
+						<li class="nav1-li-1">Sign In</li>
+						<li class="nav1-li-2">My Starbucks</li>
+						<li class="nav1-li-3">Customer Service & Ideas</li>
+						<li class="nav1-li-4">Find a Store</li>
+					</ul>
+					<button class="search-btn">
+						<img :src="require('@/assets/images/outline_search.png')">
+					</button>
+				</nav>
+				<nav class="nav2">
+					<ul>
+						<li class="nav2-li-1">COFFEE</li>
+						<li class="nav2-li-2">MENU</li>
+						<li class="nav2-li-3">STORE</li>
+						<li class="nav2-li-4">RESPONSIBILITY</li>
+						<li class="nav2-li-5">STARBUCKS REWARDS</li>
+						<li class="nav2-li-6">WHAT'S NEW</li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
 export default {
-  name: "Home",
+	name: "Home",
 };
 </script>
